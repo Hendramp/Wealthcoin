2140bf16d0544a83aba482b25b0b7b51ee065b6d
Hendramp <Hendrickramirez214@outlook.com>
2026-09-05 14:34:31 -0500

    really fixed?

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -15,7 +15,6 @@ export default function HomePage()
         <FoundationSection />
         <LibrarySection />
         <TokenomicsSection />
-        <EarlyAccessSection />
       </main>
 
       <Footer />
